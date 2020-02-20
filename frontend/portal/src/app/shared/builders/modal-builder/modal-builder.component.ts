792bc4ceb257b66f29eabc488b9be6b76b1a0964
Justin Rickard <justin@river.red>
2020-02-20 18:01:59 +0000

    Add icons

--- frontend/portal/src/app/shared/builders/modal-builder/modal-builder.component.ts
+++ frontend/portal/src/app/shared/builders/modal-builder/modal-builder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-modal-builder',
@@ -10,15 +11,19 @@ export class ModalBuilderComponent implements OnInit {
   @Input() header: string;  
   @Input() triggerButtonText: string;
   @Input() closeButtonText: string = "Cancel";
+  @Input() closeButtonIcon: string = "times-circle";
   @Input() submitButtonText: string = "Submit";
+  @Input() submitButtonIcon: string = "check-circle"
 
   @Output() submitAction = new EventEmitter<string>();
+
+  faTimes = faTimes;
+  faCheck = faCheck;
 
   show:boolean;  
 
   constructor() {
     this.show = false;
-    console.log("submitAction", this.submitAction);
   }
 
   toggleShow() {
