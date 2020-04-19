8fcc196646ca3bfe3cff19cd09e79f9fbcfe9427
Justin Rickard <justin@river.red>
2020-04-19 17:20:43 +0100

    Start adding sets array

--- frontend/portal/src/app/features/singles-result/singles-result-form/singles-result-form.component.ts
+++ frontend/portal/src/app/features/singles-result/singles-result-form/singles-result-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 import * as moment from 'moment';
 import { Lookup } from 'src/app/models/lookup';
 
@@ -13,27 +13,47 @@ export class SinglesResultFormComponent implements OnInit {
   resultsForm: FormGroup;
   @Output() submitAction = new EventEmitter<FormGroup>();
 
+  defaultBestOfSets:number = 3;
+
+  // Lookup data
   players: ILookup[];
   competitions: ILookup[];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {    
+   }
 
   ngOnInit() {
-    this.players = [new Lookup("1","Justin Rickard"), new Lookup("2", "Phil Beiken")];
-
-    let date = new FormControl(moment());
-    //let competition = new FormControl("Friendly")
+    this.players = [new Lookup("1","Justin Rickard"), new Lookup("2", "Phil Beiken")];    
     this.competitions = [new Lookup("1","Friendly"), new Lookup("2","Men's 2nd League")];
 
-
     this.resultsForm = new FormGroup({
-      date: date,
-      competition: new FormControl("1")
+      date: new FormControl(moment()),
+      competition: new FormControl(),
+      bestOfSets: new FormControl(this.defaultBestOfSets),
+      player1: new FormControl(),
+      player1Sets: this.fb.array([null, null, null]),
+      player2: new FormControl(),
+      player2Sets: this.fb.array([null, null, null]),
     });
   }
 
   submitForm() {
     console.log("Form: ", this.resultsForm);
     this.submitAction.emit(this.resultsForm)
+  }
+
+  updateBestOfSets() {
+    const player1Sets = this.resultsForm.get('player1Sets') as FormArray;
+    const player2Sets = this.resultsForm.get('player2Sets') as FormArray;
+   
+    player1Sets.setValue([]);
+    player2Sets.setValue([]);
+
+    const totalSets = this.resultsForm.get('bestOfSets').value as number;
+
+    for(var i:number=0; i < totalSets; i++) {
+      player1Sets.push(null);
+      player2Sets.push(null);
+    }
   }
 }
